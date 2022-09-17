e9f13688276dfafa90412baf26208488ea5452e6
sally nader <SALLY.NADER.AHMED@hotmail.com>
2022-09-17 13:48:07 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,27 +26,6 @@ module "bastion-host" {
   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
-module "eks-cluster" {
-  source             = "./eks-cluster"
-  nfs                = module.nfs.efs
-  desired_capacity   = 3
-  max_size           = 4
-  min_size           = 3
-  key_name           = "ec2-key"
-  key                = aws_key_pair.ec2-key.key_name
-  vpc_id             = module.vpc.vpc_main.id
-  subnets_ids        = concat(module.vpc.public_subnets_id, module.vpc.private_subnets_id)
-  instance_type      = "t2.micro"
-  cluster_name       = "eks-cluster"
-  node_group_name    = "worker-nodes"
-  template_name      = "linux-eks-nodes"
-  image_id           = "ami-0022f774911c1d690"
-  bastion_id         = module.bastion-host.sg-bastion
-  user_data_file     = "../bash/script.sh"
-  subnet_ids         = module.vpc.private_subnets_id
-  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
-
 
 terraform {
   backend "s3" {
