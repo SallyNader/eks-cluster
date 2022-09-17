0ee3913640304a535e1da891b96fb67cee51cf62
sally nader <SALLY.NADER.AHMED@hotmail.com>
2022-09-17 14:45:48 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,14 @@ module "vpc" {
   aws_secret_key = var.aws_secret_key
 }
 
+module "nfs" {
+  source         = "./nfs"
+  vpc_id         = module.vpc.vpc_main.id
+  cidr_blocks    = module.vpc.vpc_main.cidr_block
+  subnet_ids     = concat(module.vpc.public_subnets_id, module.vpc.private_subnets_id)
+  aws_access_key = var.aws_access_key
+  aws_secret_key = var.aws_secret_key
+}
 
 terraform {
   backend "s3" {
