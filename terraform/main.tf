eb0fd42f115d529bd053ec86dfef7424d8834721
sally nader <SALLY.NADER.AHMED@hotmail.com>
2022-09-17 14:50:29 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,13 +4,17 @@ module "vpc" {
   aws_secret_key = var.aws_secret_key
 }
 
-module "nfs" {
-  source         = "./nfs"
-  vpc_id         = module.vpc.vpc_main.id
-  cidr_blocks    = module.vpc.vpc_main.cidr_block
-  subnet_ids     = concat(module.vpc.public_subnets_id, module.vpc.private_subnets_id)
-  aws_access_key = var.aws_access_key
-  aws_secret_key = var.aws_secret_key
+module "bastion-host" {
+  source             = "./bastion"
+  desired_capacity   = 3
+  max_size           = 4
+  min_size           = 3
+  key                = aws_key_pair.ec2-key.key_name
+  template_name      = "linux"
+  vpc_id             = module.vpc.vpc_main.id
+  instance_type      = "t2.micro"
+  image_id           = "ami-0022f774911c1d690"
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 terraform {
