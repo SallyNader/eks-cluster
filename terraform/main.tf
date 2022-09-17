e0cb8196b1f31e356defabc48bc077325028f492
sally nader <SALLY.NADER.AHMED@hotmail.com>
2022-09-17 14:56:14 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,13 @@ module "vpc" {
   aws_access_key = var.aws_access_key
   aws_secret_key = var.aws_secret_key
 }
-
+  
+module "nfs" {
+  source         = "./nfs"
+  vpc_id         = module.vpc.vpc_main.id
+  cidr_blocks    = module.vpc.vpc_main.cidr_block
+  subnet_ids     = concat(module.vpc.public_subnets_id, module.vpc.private_subnets_id)
+}
 module "bastion-host" {
   source             = "./bastion"
   desired_capacity   = 3
