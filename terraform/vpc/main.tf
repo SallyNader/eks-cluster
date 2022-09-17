9217878ac490260cfe1ebe632e66df4ef7c9e86f
sally nader <SALLY.NADER.AHMED@hotmail.com>
2022-09-17 12:53:34 +0200

    Update main.tf

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    key            = "terraform/key"
+    key            = "terraform/vpc"
     bucket         = "eks-tf-s3-state"
     region         = "us-east-1"
     dynamodb_table = "eks-tf-state"
