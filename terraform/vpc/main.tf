5eb0fa8844b3d246ad035e5439ca9dc9d61458d6
sally nader <SALLY.NADER.AHMED@hotmail.com>
2022-09-17 13:47:21 +0200

    Delete main.tf

--- terraform/vpc/main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    key            = "terraform/vpc"
-    bucket         = "eks-tf-s3-state"
-    region         = "us-east-1"
-    dynamodb_table = "eks-tf-state"
-  }
-}
