7ab8c149666dcfb625b0a3c5f8c978d5e3da1a68
sally nader <SALLY.NADER.AHMED@hotmail.com>
2022-09-17 12:02:55 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,7 @@ module "eks-cluster" {
 
 terraform {
   backend "s3" {
+    key            = "terraform/key"
     bucket         = "eks-tf-s3-state"
     region         = "us-east-1"
     dynamodb_table = "eks-tf-state"
